6f1d6199ae9f2d6f2a05177690235ab552681327
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-10-19 15:57:44 +0100

    Add release_bike method to boris_bikes.rb

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,4 +1,10 @@
 require './lib/boris_bikes'
 
 describe DockingStation do
+
+it 'expects a DockingStation instance to respond_to the method release_bike' do
+  docking_station = DockingStation.new
+  expect { docking_station.release_bike }.not_to raise_error
+end
+
 end
