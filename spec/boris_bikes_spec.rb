75e49120bb1179a54aecaa9c918b947c92a43517
Mohammed Salah <mohammed_salah@icloud.com>
2021-10-19 17:14:45 +0100

    Inserted bike.spec file

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -7,4 +7,10 @@ it 'expects a DockingStation instance to respond_to the method release_bike' do
   expect { docking_station.release_bike }.not_to raise_error
 end
 
+it 'expects station_release.bike to return a new instance of the Bike class' do
+  docking_station = DockingStation.new
+  expect DockingStation.release_bike.working?.to eq true
+end
+
+
 end
